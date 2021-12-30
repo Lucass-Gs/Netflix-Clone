69b9080f2b3e10eb87cd70eb7b1aa37919f3d41d
Lucas Santos <84105649+Lucass-Gs@users.noreply.github.com>
2021-12-30 02:23:41 -0300

    Projeto Front-end

---
+++ NetflixClone/assets/js/scripts.js
@@ -0,0 +1,19 @@
+function abrirModal() {
+    document.getElementById('modal').style.top = "0";
+
+}
+
+function fecharModal() {
+    document.getElementById('modal').style.top = "-300%";
+
+}
+
+function abrirInfo() {
+    document.getElementById('info').style.top = "0";
+
+}
+
+function fecharInfo() {
+    document.getElementById('info').style.top = "-300%";
+
+}
